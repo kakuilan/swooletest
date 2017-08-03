07e71145a4e34a9c32eb997cb6f290cd50b06907
kakuilan <kakuilan@163.com>
2017-08-04 00:10:41 +0800

    dev

--- apps/temp.php
+++ apps/temp.php
@@ -340,3 +340,9 @@ function getMimes() {
 }
 
 
+function getMillisecond()
+{
+    list($t1, $t2) = explode(' ', microtime());
+    return (float)sprintf('%.0f', (floatval($t1) + floatval($t2)) * 1000);
+}
+
